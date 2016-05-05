9de34298d97661e2614156d2020ea62411d02ab4
Yovanny Zeballos Medina <yovanny_jzm@hotmail.com>
2016-05-05 00:03:01 -0500

    Consumiendo el servicio

--- DemoConsoleServicio/Program.cs
+++ DemoConsoleServicio/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DemoConsoleServicio.FootballServices;
 
 namespace DemoConsoleServicio
 {
@@ -10,6 +11,19 @@ namespace DemoConsoleServicio
     {
         static void Main(string[] args)
         {
+            InfoSoapTypeClient proxy = new InfoSoapTypeClient("InfoSoap");
+            Console.WriteLine("EQUIPOS");
+            Console.WriteLine("========");
+
+            tTeamInfo[] equipos = proxy.Teams();
+
+           foreach(var item in equipos){
+               Console.WriteLine(item.sName);
+           }
+
+           Console.ReadLine();
+
+
         }
     }
 }
